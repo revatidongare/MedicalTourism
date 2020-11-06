decad75cc3d251cea709be8aa58a2c6dcc670444
Vaishnavi Baporikar <vaishnavibaporikar10@gmail.com>
2020-11-06 15:36:09 +0530

    Video upload
    moving  videos

---
+++ includes/navbar.php
@@ -0,0 +1,71 @@
+   <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+		<div class="top-area">
+			<div class="container">
+				<div class="row">
+					<div class=" col-lg-4 col-sm-4 col-md-4">
+					<p class="bold text-left"><i class="fa fa-clock-o"></i> Monday-Saturday, 8am to 10pm </p>
+					</div>
+					<div class="col-lg-8 col-sm-4 col-md-4" align="right">
+					
+					<a class="bold text-right" data-toggle="modal" data-target="#exampleModal">
+  						<b>Login</b>
+					</a>
+
+					<b><a class="bold text-right" style="padding:10px";>Contact</a></b>
+					</div>
+				</div>
+			</div>
+		</div>
+        <div class="container navigation">
+		
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="index.php">
+                   <img src="img/photo/Logo.JPG" alt="" width="120" height="100" />
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+			  <ul class="nav navbar-nav">
+				<li class="active"><a href="index.php">Home</a></li>
+				<li><a href="doctor.php">Our Doctors</a></li>
+				<li><a href="speciality.php">Medical Tourism</a></li>
+				<li><a href="#facilities">International Forum</a></li>
+				<!--<li><a href="#pricing">Pricing</a></li>-->
+				<!--<li class="dropdown">
+				  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="badge custom-badge red pull-right">Extra</span>More <b class="caret"></b></a>
+				  <ul class="dropdown-menu">
+					<li><a href="index.html">Home form</a></li>
+					<li><a href="index-video.html">Home video</a></li>
+					<li><a href="index-cta.html">Home CTA</a></li>
+					<li><a href="https://bootstrapmade.com">Download</a></li>
+				  </ul>
+				</li-->			  </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+
+        <!-- /.container -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
+    </nav>
---
+++ speciality.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Welcome to page</h1>
+<?php
+echo "My first PHP script!";
+?>
+<p> Comming Soo </p>
+</body>
+</html>
