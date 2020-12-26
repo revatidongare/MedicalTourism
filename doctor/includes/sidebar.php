5954f911642af79a31327b336815a86297df81de
Vaishnavi Baporikar <vaishnavibaporikar10@gmail.com>
2020-12-26 12:09:14 +0530

    Changes done in Doctor file

--- doctor/includes/sidebar.php
+++ doctor/includes/sidebar.php
@@ -90,7 +90,7 @@
 												</a>
 											</li>
 											<li>
-												<a href="index-2.php">
+												<a href="logout.php">
 													<i class="fas fa-sign-out-alt"></i>
 													<span>Logout</span>
 												</a>
